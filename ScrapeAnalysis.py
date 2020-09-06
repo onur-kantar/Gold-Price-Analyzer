aaf7692aedf88e599783bb7772d61edad2e5778b
Onur Kantar <onurkantr@gmail.com>
2020-09-06 16:11:40 +0300

    edit and new dataset

--- ScrapeAnalysis.py
+++ ScrapeAnalysis.py
@@ -8,8 +8,8 @@ import pandas as pd
 from datetime import datetime, date
 
 def scrape () :
-    #url1 = 'https://www.investing.com/currencies/xau-usd-historical-data'
-    url1 = 'https://www.investing.com/indices/usdollar-historical-data'
+    url1 = 'https://www.investing.com/currencies/xau-usd-historical-data'
+    #url1 = 'https://www.investing.com/indices/usdollar-historical-data'
     url2 = 'https://www.gold.org/goldhub/data/gold-prices'
     chromedriver_autoinstaller.install()
     browser = webdriver.Chrome()
@@ -17,9 +17,16 @@ def scrape () :
     browser.maximize_window()
     time.sleep(5)
     
+    '''
     newYear = date(date.today().year, 1, 1)
     newYear = newYear.strftime('%m/%d/%Y')
-    print(newYear)
+    print(newYear)'''
+    
+    startDateName = date(2018,7,11)
+    startDateName = startDateName.strftime('%m/%d/%Y')
+    endDateName = date(2020,7,11)
+    endDateName = endDateName.strftime('%m/%d/%Y')
+
     
     calendar = browser.find_element_by_id('flatDatePickerCanvasHol')
     calendar.click()
@@ -27,33 +34,40 @@ def scrape () :
 
     startDate = browser.find_element_by_id('startDate')
     startDate.clear()
-    startDate.send_keys(newYear)
+    startDate.send_keys(startDateName)
+    time.sleep(1)
+    
+    endDate = browser.find_element_by_id('endDate')
+    endDate.clear()
+    endDate.send_keys(endDateName)
     time.sleep(1)
 
     calendar = browser.find_element_by_id('applyBtn')
     calendar.click()
     time.sleep(1)
+    
+
 
     df = pd.read_html(browser.page_source)[0]
-    
     df['Date'] =  pd.to_datetime(df['Date'], format='%b %d, %Y').dt.date
+    '''
 
-    startDate = date(2020,2,11)
-    endDate= date(2020,7,12)
     HighestOfYear = df["High"].max()
     LowestOfYear = df["Low"].min()
 
     mask = (df['Date'] > startDate) & (df['Date'] <= endDate)
     df = df.loc[mask].reset_index(drop=True)
+    '''
 
-    df['HighestOfYear'] = HighestOfYear
-    df['LowestOfYear'] = LowestOfYear   
+    '''df['HighestOfYear'] = HighestOfYear
+    df['LowestOfYear'] = LowestOfYear   '''
+    
     
     classes = []
     classes = np.where(df['Open'] < df['Price'], '1', (np.where(df['Open'] > df['Price'], '-1', '0')))
-    df['target'] = classes
+    df['Target'] = classes
     
-    df = df.drop(['Vol.'], axis=1)
+    df = df.drop(['Change %'], axis=1)
     df = df.drop(['Date'], axis=1)
 
     #df = df.drop(['Change %'], axis=1)
@@ -66,15 +80,15 @@ def scrape () :
     df = df.rename(columns={'LowestOfYear': 'DXY_LowestOfYear'})
     df = df.rename(columns={'target': 'DXY_Target'})
 
-    df.to_csv('DXY.csv', index = False)
+    df.to_csv('2.csv', index = False)
     xau = pd.read_csv('XAU.csv', encoding = 'UTF-8', delimiter=',')
     
     frames = [xau, df]
     result = pd.concat(frames, axis = 1)
 
-    result.to_csv('XAU_DXY.csv', index = False)
+    result.to_csv('2.csv', index = False)
 
-    print(result) 
+    print(df) 
     
 
 
